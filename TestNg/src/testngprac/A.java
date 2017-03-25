288e89603f05e5cb26cd31247c8ee0528ac05d6b
Himanshu <Himanshu@Himanshu-XPS>
2017-03-25 11:39:30 +0530

    Testcase_001 added

---
+++ TestNg/src/testngprac/A.java
@@ -0,0 +1,27 @@
+package testngprac;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+
+public class A {
+	
+	@BeforeMethod
+	public void Bmet()
+	{
+		System.out.println("Before");
+	}
+	
+	@AfterMethod
+	public void Amet()
+	{
+		System.out.println("After");
+	}
+	
+	@Test
+	public void test()
+	{
+		System.out.println("Hello World");
+	}
+
+}
